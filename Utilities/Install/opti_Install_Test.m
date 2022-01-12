747acceae45c189082c4931cac24af026d779c3e
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2022-01-12 20:46:04 +0100

    minor correction

--- Utilities/Install/opti_Install_Test.m
+++ Utilities/Install/opti_Install_Test.m
@@ -45,7 +45,7 @@ for t = 1:length(tests)
         if(verb); fprintf(' %s ', msolvers{i}); end
         for j = 1:noP
             if(verb); fprintf(' %d ', j); end
-            if(moreverb);
+            if(moreverb)
                 Opt = opti(probs{j},optiset('solver',msolvers{i},'warnings','off','display','iter'));
             else
                 Opt = opti(probs{j},optiset('solver',msolvers{i},'warnings','off','display','off'));
