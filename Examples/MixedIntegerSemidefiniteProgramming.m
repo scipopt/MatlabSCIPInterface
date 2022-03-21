15fe5c63f8fa0090dd98168509299cc07a7af6b4
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2022-03-21 14:45:38 +0100

    extend example

--- Examples/MixedIntegerSemidefiniteProgramming.m
+++ Examples/MixedIntegerSemidefiniteProgramming.m
@@ -1,12 +1,9 @@
 %% Mixed-Integer Semidefinite Programming Examples
 %
 % This file contains a number of MISDP problems and demonstrates how to
-% solve them using the OPTI Toolbox.
+% solve them using the OcdPTI Toolbox.
 %
 % Copyright (C) 2022 Marc Pfetsch
-
-clc
-clear
 
 no = 1;
 misdp_tprob = cell(no,1);
@@ -24,30 +21,11 @@ A1 = [0 1 0 0; 1 0 0 0; 0 0 0 0; 0 0 0 0];
 A2 = [0 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 0];
 sdp = sparse([C(:) A0(:) A1(:) A2(:)]);
 xtype = 'III';
-misdp = opti('f',f,'lin',A,lhs,rhs,'sdcone',sdp,'int',xtype);
 
-% Call solve to solve the problem:
-[x,fval,exitflag,info] = solve(misdp)
-
-clc
-% ------------------------------------------------------------
 % demonstrate how to write the constructed problem to a file
-opts = optiset('probfile','testmisdp.cip');
+opts = optiset('probfile','testmisdp.cip','display','iter');
 
 misdp = opti('f',f,'lin',A,lhs,rhs,'sdcone',sdp,'int',xtype,'options',opts);
 
 % again solve the problem - this will only write the problem:
 [x,fval,exitflag,info] = solve(misdp);
-
-clc
-% ------------------------------------------------------------
-% demonstrate how to use parameters
-opts = optiset('display','iter');
-% add SCIP specific parameter:
-solverOpts = {'misc/solvesdps',0};
-opts.solverOpts = solverOpts;
-
-misdp = opti('f',f,'lin',A,lhs,rhs,'sdcone',sdp,'int',xtype,'options',opts);
-
-% again solve the problem:
-[x,fval,exitflag,info] = solve(misdp)
