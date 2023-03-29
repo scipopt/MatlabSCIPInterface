691aeb27f8b4d0f052bd3a4dd3049f84f0a95a17
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2023-03-29 21:58:48 +0200

    cleanup parameters moved to optiset

--- Utilities/opti/scipset.m
+++ Utilities/opti/scipset.m
@@ -1,7 +1,7 @@
 function options = scipset(varargin)
 %SCIPSET  Create or alter the options for Optimization with SCIP
 %
-% options = scipset('param1',value1,'param2',value2,...) creates an SCIP
+% options = scipset('param1',value1,'param2',value2,...) creates a SCIP
 % options structure with the parameters 'param' set to their corresponding
 % values in 'value'. Parameters not specified will be set to the SCIP
 % default.
@@ -26,8 +26,8 @@ if ((nargin == 0) && (nargout == 0))
 end
 
 % names and defaults
-Names = {'scipopts','globalEmphasis','heuristicsEmphasis','presolvingEmphasis','separatingEmphasis','cipfile','gamsfile','testmode'};
-Defaults = {[],'default','default','default','default',[],[],0};
+Names = {'globalEmphasis','heuristicsEmphasis','presolvingEmphasis','separatingEmphasis'};
+Defaults = {'default','default','default','default'};
 
 % enter and check user args
 try
@@ -40,22 +40,13 @@ end
 % Check a field contains correct data type.
 function checkfield(field,value)
 switch lower(field)
-    % Scalar 0/1
-    case 'testmode'
-        err = opticheckval.checkScalar01(value,field);
-    % char array
-    case {'gamsfile','cipfile'}
-        err = opticheckval.checkChar(value,field);    
     % specific emphasis
     case {'heuristicsemphasis', 'presolvingemphasis', 'separatingemphasis'}
         err = opticheckval.checkValidString(value, field, {'aggressive','fast','off','default'});
     % global emphasis
     case 'globalemphasis'
         err = opticheckval.checkValidString(value, field, {'counter','cpsolver','easycip','feasibility','hardlp','optimality','default'});
-    % cell array
-    case 'scipopts'
-        err = opticheckval.checkCell2Col(value,field); 
-    otherwise  
+    otherwise
         err = MException('OPTI:SetFieldError','Unrecognized parameter name ''%s''.', field);
 end
 if(~isempty(err)), throw(err); end
@@ -63,21 +54,10 @@ if(~isempty(err)), throw(err); end
 
 % Print out fields with defaults.
 function printfields()
-fprintf('           scipopts: [ Set SCIP options using a 2D cell format: {''name1'', val1; ''name2'', val2; ...} (See Below) {[]} ] \n');
 fprintf('     globalEmphasis: [ Global predefined parameter setting for emphasis: {''default''}, ''counter'', ''cpsolver'', ''easycip'', ''feasibility'', ''hardlp'', ''optimality'' ] \n');
 fprintf(' heuristicsEmphasis: [ Heuristics emphasis predefined parameter setting (overrides global): {''default''}, ''aggressive'', ''fast'', ''off'' ] \n');
 fprintf(' presolvingEmphasis: [ Presolving emphasis predefined parameter setting (overrides global): {''default''}, ''aggressive'', ''fast'', ''off'' ] \n');
 fprintf(' separatingEmphasis: [ Separating emphasis predefined parameter setting (overrides global): {''default''}, ''aggressive'', ''fast'', ''off'' ] \n');
-fprintf('            cipfile: [ Write SCIP model to CIP file (will skip solving): {[]}, ''filename'' (Ensure Display is Off) ] \n');
-fprintf('           gamsfile: [ Write SCIP model to GAMS file (will skip solving): {[]}, ''filename'' (Ensure Display is Off) ] \n');
-fprintf('           testmode: [ Validate the nonlinear function generation (will skip solving): {0}, 1 ] \n');
-fprintf('\n');
-
-fprintf(' To set SCIP options not available via optiset, you may now set any available SCIP option using the ''scipopts'' field:\n');
-fprintf(' - For a list of all available options, see https://scipopt.org/doc/html/PARAMETERS.php.\n');
-fprintf(' - For example, to limit SCIP to the first feasible solution, use "scipset(''scipopts'',{''limits/solutions'',1})".\n');
-fprintf(' - Note:\n    - Boolean, Int, LongInt and Real parameters are all assumed MATLAB doubles, and converted internally.\n');
-fprintf('    - scipopts options take priority over all optiset options and override any emphasis settings.\n');
 fprintf('\n');
 
 
