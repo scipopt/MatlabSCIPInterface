f0e0c4885fd239fa0cfec2d1a4ef5f3cee027de9
Marc Pfetsch <pfetsch@mathematik.tu-darmstadt.de>
2023-03-29 22:00:32 +0200

    cleanup parameters, fix documentation

--- Utilities/opti/optiset.m
+++ Utilities/opti/optiset.m
@@ -13,9 +13,14 @@ function options = optiset(varargin)
 % options = optiset() creates an options structure with all fields set to
 % OPTI defaults.
 %
+% solverOpts can pass parameters to the solvers, e.g., by
+% sopts = {'limits/solutions',1};
+% opts = optiset('solverOpts',sopts);
+%
 % optiset() prints a list of all possible fields and their function.
 
-%   Copyright (C) 2011 Jonathan Currie (IPL)
+% Copyright (C) 2011 Jonathan Currie (IPL)
+% Copyright (C) 2023 Marc Pfetsch
 
 % Print out possible values of properties.
 if((nargin == 0) && (nargout == 0))
@@ -23,11 +28,11 @@ if((nargin == 0) && (nargout == 0))
     return
 end
 
-%Names and Defaults
-Names = {'solver';'maxiter';'maxfeval';'maxnodes';'maxtime';'tolrfun';'tolafun';'tolint';'solverOpts';'dynamicOpts';'iterfun';'warnings';'display';'probfile';'presolvedfile';'derivCheck'};
-Defaults = {'auto';1e8;1e4;1e8;3600;1e-6;1e-6;1e-6;[];[];[];'critical';'off';[];[];'off'};
+% Names and Defaults
+Names = {'solver';'maxiter';'maxnodes';'maxtime';'tolrfun';'tolafun';'tolint';'solverOpts';'warnings';'display';'probfile';'presolvedfile'};
+Defaults = {'auto';1e8;1e8;3600;1e-6;1e-6;1e-6;[];'off';'iter';[];[]};
 
-%Enter and check user args
+% Enter and check user args
 try
     options = opticheckset(Names,Defaults,@checkfield,varargin{:});
 catch ME
@@ -47,26 +52,21 @@ function checkfield(field,value)
 switch lower(field)
     %Scalar non negative double
     case {'tolafun','tolrfun','tolint'}
-        err = opticheckval.checkScalarNonNeg(value,field);    
+        err = opticheckval.checkScalarNonNeg(value,field);
     %Scalar non zero double
     case 'maxtime'
-        err = opticheckval.checkScalarGrtZ(value,field);  
+        err = opticheckval.checkScalarGrtZ(value,field);
     %Scalar non zero integer
-    case {'maxiter','maxfeval','maxnodes'}
+    case {'maxiter','maxnodes'}
         err = opticheckval.checkScalarIntGrtZ(value,field);
     %Struct
-    case {'solveropts','dynamicopts'}
+    case {'solveropts'}
         err = opticheckval.checkStruct(value,field);
-    %Function Handle
-    case 'iterfun'
-        err = opticheckval.checkFunHandle(value,field);
     %Other misc
     case 'display'
-        err = opticheckval.checkValidString(value,field,{'off','iter','final'});
-    case 'warnings'        
+        err = opticheckval.checkValidString(value,field,{'off','iter','full'});
+    case 'warnings'
         err = opticheckval.checkValidString(value,field,{'on','critical','off','all','none'});
-    case 'derivcheck'        
-        err = opticheckval.checkValidString(value,field,{'on','off'});
     case 'solver'
         err = opticheckval.checkOptiSolver(value,field);
     case {'probfile'}
@@ -80,7 +80,7 @@ if(~isempty(err)), throw(err); end
 
 
 function printfields()
-%Print out fields with defaults
+% print out fields with defaults
 
 solvers = [{'AUTO'} optiSolver('all')];
 len = length(solvers);
@@ -91,23 +91,19 @@ for i = 1:len
     else
         str = [str solvers{i}]; %#ok<AGROW>
     end
-end   
+end
 str = regexprep(str,'AUTO','{AUTO}');
 
 fprintf('\n OPTISET Fields:\n');
 fprintf(['            solver: [ ' str ' ] \n']);
 fprintf('           maxiter: [ Maximum Solver Iterations {1.5e3} ] \n');
-fprintf('          maxfeval: [ Maximum Function Evaluations {1e4} ] \n');
 fprintf('          maxnodes: [ Maximum Integer Solver Nodes {1e4} ] \n');
 fprintf('           maxtime: [ Maximum Solver Evaluation Time {1e3s} ] \n');
 fprintf('           tolrfun: [ Relative Function Tolerance {1e-7} ] \n');
 fprintf('           tolafun: [ Absolute Function Tolerance {1e-7} ] \n');
 fprintf('            tolint: [ Absolute Integer Tolerance {1e-5} ] \n');
 fprintf('        solverOpts: [ Solver Specific Options Structure (e.g. for SCIP(SDP))] \n');
-fprintf('       dynamicOpts: [ Dynamic Optimization Options Structure ] \n');
-fprintf('           iterfun: [ Iteration Callback Function, stop = iterfun(iter,fval,x) {} ] \n');
 fprintf('          warnings: [ ''all'' or {''critical''} or ''none'' ] \n');
-fprintf('           display: [ {''off''}, ''iter'', ''final'' ] \n');
+fprintf('           display: [ ''off'' or ''iter'' or ''full'' ] \n');
 fprintf('          probfile: [ Write problem to file: {[]}, ''filename''] \n');
 fprintf('     presolvedfile: [ Write presolved problem to file: {[]}, ''filename''] \n');
-fprintf('        derivCheck: [ Derivative Checker {''off''}, ''on'' ] \n');
